76cfde38e294c72c06c25b174bc92f8330d2e272
Raunaq Bhirangi <raunaqbhirangi@nyu.edu>
2024-11-21 11:41:18 -0500

    move fisheye server into same file as camera server

--- camera_server.py
+++ camera_server.py
@@ -1,12 +1,12 @@
-from frankateach.camera_server import RealsenseServer
-from frankateach.constants import HOST, CAM_PORT
+from frankateach.camera_server import FishEyeServer, RealsenseServer
+from frankateach.constants import FISHEYE_CAM_PORT, HOST, CAM_PORT
 import hydra
 import argparse
 
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="camera")
 def main(cfg):
-    cam_configs = [
+    realsense_cam_configs = [
         argparse.Namespace(
             cam_serial_num=cfg.cam_serial_numbers[i],
             depth=cfg.cam_config.depth,
@@ -18,13 +18,30 @@ def main(cfg):
         for i in range(len(cfg.cam_serial_numbers))
     ]
 
-    server = RealsenseServer(
+    realsense_server = RealsenseServer(
         host=HOST,
         cam_port=CAM_PORT,
-        cam_configs=cam_configs,
+        cam_configs=realsense_cam_configs,
     )
 
-    server._init_camera_threads()
+    fisheye_cam_configs = [
+        argparse.Namespace(
+            cam_serial_num=serial_num,
+            fps=cfg.fisheye_cam_config.fps,
+            height=cfg.fisheye_cam_config.height,
+            width=cfg.fisheye_cam_config.width,
+        )
+        for _, serial_num in range(len(cfg.fisheye_cam_numbers))
+    ]
+
+    fisheye_server = FishEyeServer(
+        host=HOST,
+        cam_port=FISHEYE_CAM_PORT,
+        cam_configs=fisheye_cam_configs,
+    )
+
+    realsense_server._init_camera_threads()
+    fisheye_server._init_camera_threads()
 
 
 if __name__ == "__main__":
