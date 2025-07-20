c439bfae0674b89fffe9c0528d794b08b38f8b6f
joliachen <zc2745@nyu.edu>
2025-07-20 16:06:29 -0400

    update

--- teleop.py
+++ teleop.py
@@ -1,41 +1,53 @@
 import hydra
 from multiprocessing import Process
-from frankateach.teleoperator import FrankaOperator
+from hydra.utils import instantiate
+from frankateach.teleoperator_stick import FrankaOperator
 from frankateach.oculus_stick import OculusVRStickDetector
-from frankateach.constants import HOST, VR_CONTROLLER_STATE_PORT
 
 
-def start_teleop(init_gripper_state="open", teleop_mode="robot", home_offset=None):
-    operator = FrankaOperator(
-        init_gripper_state=init_gripper_state,
-        teleop_mode=teleop_mode,
-        home_offset=home_offset,
-    )
+def start_oculus_stick(cfg):
+    detector = instantiate(cfg.oculus_stick_detector)
+    detector.stream()
+    
+
+def start_oculus_hand(cfg):
+    detector = instantiate(cfg.oculus_hand_detector)
+    detector.stream()
+
+def start_hand_transform(cfg):
+    transformer = instantiate(cfg.hand_transform)
+    transformer.stream()
+    
+def start_teleop_stick(cfg):
+    operator = instantiate(cfg.stick_operators)
     operator.stream()
 
+def start_teleop_hand(cfg):
+    operator = instantiate(cfg.hand_operators)
+    operator.stream()
 
-def start_oculus_stick():
-    detector = OculusVRStickDetector(HOST, VR_CONTROLLER_STATE_PORT)
-    detector.stream()
 
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="teleop")
 def main(cfg):
-    teleop_process = Process(
-        target=start_teleop,
-        args=(
-            cfg.init_gripper_state,
-            cfg.teleop_mode,
-            cfg.home_offset,
-        ),
-    )
-    oculus_stick_process = Process(target=start_oculus_stick)
+    processes = []
+    if cfg.use_oculus_stick:
+        oculus_process = Process(target=start_oculus_stick, args=(cfg,))
+        teleop_process = Process(target=start_teleop_stick, args=(cfg,))
+    else:
+        oculus_process = Process(target=start_oculus_hand, args=(cfg,))
+        transform_process = Process(target=start_hand_transform, args=(cfg,))
+        teleop_process = Process(target=start_teleop_hand, args=(cfg,))
+        processes.append(transform_process)
+    
+    processes.append(oculus_process)
+    processes.append(teleop_process)
 
-    teleop_process.start()
-    oculus_stick_process.start()
+    for p in processes:
+        p.start()
 
-    teleop_process.join()
-    oculus_stick_process.join()
+    for p in processes:
+        p.join()
 
 
 if __name__ == "__main__":
