451d872a296942928173a62d1da3ff4f623f1f81
pianapolataa <sissi.hu@hotmail.com>
2025-10-14 16:31:45 -0400

    .

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -106,7 +106,7 @@ class RukaOperator:
            
 
         if self.start_teleop:
-            self.handler.step(transformed_hand_coords, 20)
+            self.handler.step(transformed_hand_coords, 30)
   
 
     def stream(self):
