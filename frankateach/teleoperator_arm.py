b50d0cbbadf8703c879b1d6a02634af9df637786
pianapolataa <sissi.hu@hotmail.com>
2025-10-23 11:09:03 -0400

    change arm rotation

--- frankateach/teleoperator_arm.py
+++ frankateach/teleoperator_arm.py
@@ -379,9 +379,11 @@ class FrankaArmOperator:
             # Define hand axes from initial hand frame
             hand_axes_mat = self.hand_init_H[:3, :3].copy()
             angles = self._angles_around_axes(relative_rot, hand_axes_mat)
+            print("Raw angles:", angles)
             # angles[0] = side axis, 1 = wrist axis, 2 = palm normal
             angles[2] = self._scale_angle(angles[2], -90, 90)
             angles[0] = self._scale_angle(angles[0], 0, 55)
+            print("Scaled angles:", angles)
             relative_rot_scaled = self._rot_from_hand_axes(angles, hand_axes_mat)
             ##
             
