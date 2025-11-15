2c95819dbe73967942efa45d263b285782b737e9
pianapolataa <sissi.hu@hotmail.com>
2025-11-15 00:19:46 -0500

    change data collection order for arm and hand

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -109,8 +109,8 @@ class RukaOperator:
             motor_positions = self.handler.get_command(transformed_hand_coords)
             curr_pos = self.handler.hand.read_pos()
             move_to_pos(curr_pos=curr_pos, des_pos=motor_positions, hand=self.handler.hand, traj_len=15)
-            moved_pos = self.handler.hand.read_pos()
-            self.ruka_state_socket.pub_keypoints(moved_pos, "ruka_state")
+            # moved_pos = self.handler.hand.read_pos()
+            self.ruka_state_socket.pub_keypoints(curr_pos, "ruka_state")
             self.ruka_commanded_state_socket.pub_keypoints(motor_positions, "commanded_ruka_state")
   
 
