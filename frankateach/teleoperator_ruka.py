147b1c485bb513b35203172fc9bf79ed1910e7b9
pianapolataa <sissi.hu@hotmail.com>
2026-01-14 14:31:32 -0500

    .

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -112,9 +112,10 @@ class RukaOperator:
             motor_positions = self.handler.get_command(transformed_hand_coords)
             if motor_positions is None:
                 return None
-            if (self.cnt % 5 == 0): 
-                motor_positions += np.random.normal(0, 40, size=16)
-                motor_positions = np.clip(motor_positions, np.minimum(self.handler.hand.tensioned_pos, self.handler.hand.curled_bound), np.maximum(self.handler.hand.tensioned_pos, self.handler.hand.curled_bound))
+            ##
+            # motor_positions += np.random.normal(0, 40, size=16)
+            # motor_positions = np.clip(motor_positions, np.minimum(self.handler.hand.tensioned_pos, self.handler.hand.curled_bound), np.maximum(self.handler.hand.tensioned_pos, self.handler.hand.curled_bound))
+            ##
             curr_pos = self.handler.hand.read_pos()
             self.ruka_state_socket.pub_keypoints(curr_pos, "ruka_state")
             self.ruka_commanded_state_socket.pub_keypoints(motor_positions, "commanded_ruka_state")
