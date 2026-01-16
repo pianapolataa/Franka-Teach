454b81030693204dae7f08c5f95820be93bc95b7
pianapolataa <sissi.hu@hotmail.com>
2026-01-16 08:54:07 -0500

    .

--- frankateach/teleoperator_arm.py
+++ frankateach/teleoperator_arm.py
@@ -354,7 +354,7 @@ class FrankaArmOperator:
             print("1st frame: finish receiving robot state")
             # Move to offset position
             #
-            self.home_offset = [0, 0, -0.02]
+            self.home_offset = [0, 0, -0.07]
             #
             target_pos = robot_state.pos + self.home_offset
             print(self.home_offset)
