4975dd73ed4554ff727c06cfdfb728c5e5183ee6
pianapolataa <sissi.hu@hotmail.com>
2026-01-23 09:10:53 -0500

    .

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -113,10 +113,10 @@ class RukaOperator:
             motor_positions = self.handler.get_command(transformed_hand_coords)
             if motor_positions is None:
                 return None
-            if motor_positions[7] > 2100:
+            if motor_positions[7] < 2000:
                 self.close = True
             if self.close:
-                motor_positions[7] = 2370
+                motor_positions[7] = 1900
             print(motor_positions[7])
             ##
             # if self.cnt % 3 == 0:
