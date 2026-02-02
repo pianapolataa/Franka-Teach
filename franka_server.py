0a1c58d6635871ef3bf541b79221a537d26bf963
pianapolataa <sissi.hu@hotmail.com>
2026-02-02 16:44:32 -0500

    .

--- franka_server.py
+++ franka_server.py
@@ -3,8 +3,8 @@ import hydra
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="franka_server")
 def main(cfg):
-    fs_left = FrankaServer(cfg.deoxys_config_path_left)
-    fs_left.init_server()
+    # fs_left = FrankaServer(cfg.deoxys_config_path_left)
+    # fs_left.init_server()
     fs_right = FrankaServer(cfg.deoxys_config_path_right)
     fs_right.init_server()
 
