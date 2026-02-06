b2947178a55ecb44df878ce81a3b45ac8a8ffe23
pianapolataa <sissi.hu@hotmail.com>
2026-02-06 17:17:31 -0500

    .

--- teleop.py
+++ teleop.py
@@ -54,13 +54,15 @@ def main(cfg):
     else:
         oculus_process = Process(target=start_oculus_hand, args=(cfg,))
         oculus_process_left = Process(target=start_oculus_hand_left, args=(cfg,))
-        # transform_process = Process(target=start_hand_transform, args=(cfg,))
+        transform_process = Process(target=start_hand_transform, args=(cfg,))
+        transform_process_left = Process(target=start_hand_transform_left, args=(cfg,))
         # teleop_process_left = Process(target=start_teleop_arm_left, args=(cfg,))
         # teleop_process_right = Process(target=start_teleop_arm, args=(cfg,))
         if cfg.use_hand_tracking:
             ruka_process = Process(target=start_teleop_hand, args=(cfg,))
             processes.append(ruka_process)
-        # processes.append(transform_process)
+        processes.append(transform_process)
+        processes.append(transform_process_left)
     
     processes.append(oculus_process)
     processes.append(oculus_process_left)
