4035c655f49deff6608dd6356d4da3872d207404
pianapolataa <sissi.hu@hotmail.com>
2026-02-06 15:53:04 -0500

    .

--- frankateach/network.py
+++ frankateach/network.py
@@ -31,7 +31,7 @@ def create_pull_socket(host, port):
     context = zmq.Context()
     socket = context.socket(zmq.PULL)
     socket.setsockopt(zmq.CONFLATE, 1)
-    socket.connect("tcp://{}:{}".format(host, port))
+    socket.bind("tcp://{}:{}".format(host, port))
     return socket
 
 
