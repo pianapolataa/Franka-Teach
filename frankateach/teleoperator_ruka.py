c8064a3628c13040be0d927eb6244289f713bdb0
pianapolataa <sissi.hu@hotmail.com>
2026-02-06 13:49:41 -0500

    .

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -127,7 +127,7 @@ class RukaOperator:
             self.ruka_state_socket.pub_keypoints(curr_pos, "ruka_state")
             self.ruka_commanded_state_socket.pub_keypoints(motor_positions, "commanded_ruka_state")
             print(motor_positions)
-            # motor_positions[:14] = [2136.39, 2540, 1617.,   1860.,   2011.25, 2774.63, 1682.41, 1950.,   1814.18, 2946.93, 2924.4,  1472.58,  967.88, 1545.49]
+            motor_positions[:14] = [1872.99, 2540.,   1617.,   1860.,   1792.78, 2900.,   1520.,   1950.,   1669.2, 3229.,   3120.56, 1448.99,  846.13, 1582.89]
             move_to_pos(curr_pos=curr_pos, des_pos=motor_positions, hand=self.handler.hand, traj_len=15)
   
 
