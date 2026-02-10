312e8c18576c2f56af67df9057d9b4c650a08195
pianapolataa <sissi.hu@hotmail.com>
2026-02-10 14:39:27 -0500

    hand pose for writing

--- replay_arm_data.py
+++ replay_arm_data.py
@@ -38,7 +38,7 @@ class ArmReplayer:
             print("No actions to replay.")
             return
         
-        replay_data = self.arm_trajectory[100000::10]
+        replay_data = self.arm_trajectory[100000::17]
         if len(replay_data) == 0:
             print("Error: No data left after trimming and subsampling.")
             return
@@ -47,7 +47,7 @@ class ArmReplayer:
         
         for row in replay_data:
             if first:
-                if row[0] < 0.47: 
+                if row[0] < 0.46: 
                     continue
                 else:
                     first = False
