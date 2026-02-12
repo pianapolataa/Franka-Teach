45111c410096b95a16c32f620703959a6d169eaf
pianapolataa <sissi.hu@hotmail.com>
2026-02-12 17:51:53 -0500

    fix left handedness

--- frankateach/keypoint_transform.py
+++ frankateach/keypoint_transform.py
@@ -117,8 +117,8 @@ class TransformHandPositionCoords():
                     self.frame_moving_average_queue, 
                     self.moving_average_limit
                 )
-                # if self.hand == 'left':
-                #     self.averaged_hand_frame[:, 2] *= -1
+                if self.hand == 'left':
+                    self.averaged_hand_frame[1] *= -1
                 self.transformed_keypoint_publisher.pub_keypoints(self.averaged_hand_coords, 'transformed_hand_coords')
                 if data_type == 'absolute':
                     self.transformed_keypoint_publisher.pub_keypoints(self.averaged_hand_frame, 'transformed_hand_frame')
