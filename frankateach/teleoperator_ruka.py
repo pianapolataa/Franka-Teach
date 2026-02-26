ab3ae066ce1e6f431d50a8f316a16e4fdd77aef2
pianapolataa <sissi.hu@hotmail.com>
2026-02-26 15:09:54 -0500

    v2

--- frankateach/teleoperator_ruka.py
+++ frankateach/teleoperator_ruka.py
@@ -79,8 +79,8 @@ class RukaOperator:
         return reset_stat
 
     def _init_hand(self):
-        # self.handler = DexRukav2Handler(urdf_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets/robot.urdf", config_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets/dex_retarget.yml")
-        self.handler = DexRukav2Handler(urdf_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets_v1/Hand_Assembly_URDF.urdf", config_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets_v1/dex_retarget_v1.yml")
+        self.handler = DexRukav2Handler(urdf_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets/robot.urdf", config_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets/dex_retarget.yml")
+        # self.handler = DexRukav2Handler(urdf_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets_v1/Hand_Assembly_URDF.urdf", config_path="/home_shared/grail_sissi/BAKU/baku/vr-hand-tracking/Franka-Teach/RUKA/assets_v1/dex_retarget_v1.yml")
         self.cnt = 0
         print("init")
     
