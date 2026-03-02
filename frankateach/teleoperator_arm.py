bca111ad82b6be5b177863b266c99e13d62418a2
pianapolataa <sissi.hu@hotmail.com>
2026-03-02 18:21:53 -0500

    add left hand

--- frankateach/teleoperator_arm.py
+++ frankateach/teleoperator_arm.py
@@ -346,8 +346,8 @@ class FrankaArmOperator:
             self.resolution_scale = 0.6
             
         offset_rot_angle = 0
-        if self.hand == 'left':
-            offset_rot_angle = np.pi
+        # if self.hand == 'left':
+        #     offset_rot_angle = np.pi
         if self.is_first_frame:
             self.cnt = 0
             wrist_state = self._get_hand_frame()
