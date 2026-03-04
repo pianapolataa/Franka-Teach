953446326c3c1623d731d6f56ebdde6147523646
pianapolataa <sissi.hu@hotmail.com>
2026-03-03 19:35:33 -0500

    change left

--- frankateach/teleoperator_arm.py
+++ frankateach/teleoperator_arm.py
@@ -350,18 +350,18 @@ class FrankaArmOperator:
             offset_rot_angle = np.pi
         if self.is_first_frame:
             self.cnt = 0
-            wrist_state = self._get_hand_frame()
-            while  wrist_state is None:
-                wrist_state = self._get_hand_frame()
-                return None
+            # wrist_state = self._get_hand_frame()
+            # while  wrist_state is None:
+            #     wrist_state = self._get_hand_frame()
+            #     return None
             
-            rotated_frame = self._rotate_frame(np.pi, wrist_state)
-            rotated_frame = self._orthonormalize_frame(rotated_frame)
-            self.hand_init_H = self._turn_frame_to_homo_mat(rotated_frame)
-            offset_frame = self._rotate_frame(offset_rot_angle, wrist_state)
-            offset_frame = self._orthonormalize_frame(offset_frame)
-            self.hand_init_offset_H = self._turn_frame_to_homo_mat(offset_frame)
-            # self.hand_init_offset_H = self._turn_frame_to_homo_mat(wrist_state)
+            # rotated_frame = self._rotate_frame(np.pi, wrist_state)
+            # rotated_frame = self._orthonormalize_frame(rotated_frame)
+            # self.hand_init_H = self._turn_frame_to_homo_mat(rotated_frame)
+            # offset_frame = self._rotate_frame(offset_rot_angle, wrist_state)
+            # offset_frame = self._orthonormalize_frame(offset_frame)
+            # self.hand_init_offset_H = self._turn_frame_to_homo_mat(offset_frame)
+            # # self.hand_init_offset_H = self._turn_frame_to_homo_mat(wrist_state)
 
             print("Resetting robot..")
             action = FrankaAction(
